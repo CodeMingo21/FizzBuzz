69a1b05fead67d573c8f3352c2a6d28008012491
Tavaris Mingo <tmingo21@gmail.com>
2024-07-28 11:27:20 +0200

    Added FizzBuzz

---
+++ PF_Lesson_1.cs
@@ -0,0 +1,26 @@
+﻿// PF_Lesson_1_Exercise_1
+
+
+for (int i = 1; i <= 100; i++)
+{
+    
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+      
+    
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
